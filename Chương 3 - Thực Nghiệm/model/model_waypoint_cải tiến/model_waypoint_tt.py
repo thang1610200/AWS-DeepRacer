30b2818078ddf305ec892b55386fc43e6c33e505
Your Name <20110568@student.hcmute.edu.vn>
2022-12-14 22:18:03 +0700

    update

--- Chương 3 - Thực Nghiệm/model/model_waypoint_cải tiến/model_waypoint_tt.py
+++ Chương 3 - Thực Nghiệm/model/model_waypoint_cải tiến/model_waypoint_tt.py
@@ -20,78 +20,80 @@ def reward_function(params):
     track_width = params['track_width'] #chiều rộng của đường đua
     waypoints = params['waypoints'] #Tọa độ (x,y) dọc theo trung tâm của đường đua
 
-    # Hàm này sẽ xác định các góc cua trong tương lai mà xe sẽ tới
+    # Hàm này dùng để xác định có khúc cua phía trước hay không
     def identify_corner(waypoints, closest_waypoints, future_step):
 
-        # Tính hướng của đường trung tâm của đường đua hiện tại
+        # Tính hướng dõi của xe hiện tại 
         point_prev = waypoints[closest_waypoints[0]]
         point_next = waypoints[closest_waypoints[1]]
 
-        #Tính hướng của đường trung tâm của đường đua tương lai mà xe sẽ tới trong 7 bước tiếp theo
+        # Tính hướng theo dõi của xe trong tương lai (xem khoảng cách 7 bước tiếp theo)
         point_future = waypoints[min(len(waypoints) - 1, 
                                      closest_waypoints[1] + future_step)]
 
-        # Calculate headings to waypoints
+        # Góc độ hiện tại của xe so với đường đua
         heading_current = math.degrees(math.atan2(point_prev[1] - point_next[1], 
                                                  point_prev[0] - point_next[0]))
+
+
+        # # Góc độ tương lai của xe so với đường đua
         heading_future = math.degrees(math.atan2(point_prev[1] - point_future[1], 
                                                  point_prev[0]-point_future[0]))
 
-        # Calculate the difference between the headings
+        # tính độ lệch giữa hiện tại và tương lai
         diff_heading = abs(heading_current - heading_future)
 
-        # Check we didn't choose the reflex angle
         if diff_heading > 180:
             diff_heading = 360 - diff_heading
 
-        # Calculate distance to further waypoint
+        # Tính khoảng cách đến điểm tham chiếu xa hơn
         dist_future = np.linalg.norm([point_next[0] - point_future[0],
                                       point_next[1] - point_future[1]])  
 
         return diff_heading, dist_future
 
-
+    # lựa chọn tốc độ phù hợp với mỗi quãng đường
     def select_speed(waypoints, closest_waypoints, future_step, mid_step):
 
-        # Identify if a corner is in the future
+        # lấy 2 giá trị trả về của hàm identify_corner khoảng cách là 7 bước
         diff_heading, dist_future = identify_corner(waypoints, 
                                                     closest_waypoints, 
                                                     future_step)
 
+        #Nếu độ lệch nhỏ hơn 10 độ
         if diff_heading < TURN_THRESHOLD:
-            # If there's no corner encourage going faster
+            # thì sẽ đi nhanh hơn
             go_fast = True
         else:
-            if dist_future < DIST_THRESHOLD:
-                # If there is a corner and it's close encourage going slower
+            if dist_future < DIST_THRESHOLD: # Nếu xem 1.2 m có khúc cua nào không
+                # Nếu có một góc cua và nó gần sẽ đi chậm hơn
                 go_fast = False
             else:
-                # If the corner is far away, re-assess closer points
+                # lấy 2 giá trị trả về của hàm identify_corner khoảng cách là 4 bước
                 diff_heading_mid, dist_mid = identify_corner(waypoints, 
                                                              closest_waypoints, 
                                                              mid_step)
-
+               # #Nếu độ lệch nhỏ hơn 10 độ
                 if diff_heading_mid < TURN_THRESHOLD:
-                    # If there's no corner encourage going faster
+                    # Nếu không có góc cua sẽ đi nhanh
                     go_fast = True
                 else:
-                    # If there is a corner and it's close encourage going slower
+                    # Nếu có một góc cua và nó gần sẽ đi chậm hơn
                     go_fast = False
 
         return go_fast
 
-    # Strongly discourage going off track
+    # Nếu xe ko trong đường đua or lệch hướng
     if not all_wheels_on_track or is_offtrack:
-        reward = 1e-3
+        reward = 1e-3  
         return float(reward)
     
-    # Give higher reward if the car is closer to centre line and vice versa
-    # 0 if you're on edge of track, 1 if you're centre of track
+    # Thưởng càng cao nếu xe càng gần vạch giữa và ngược lại
+     # 0 nếu đang ở rìa đường đua, 1 nếu ở giữa đường đua
     reward = 1 - (distance_from_center/(track_width/2))**(1/4) + progress/steps
 
     go_fast = select_speed(waypoints, closest_waypoints, FUTURE_STEP, MID_STEP)
 
-    # Implement speed incentive
     if go_fast and speed > SPEED_THRESHOLD:
         reward += 0.5
 
